1e3aec9b5b5ae75861338ca3bdc0412fe98f46b3
Tuscann <fbinnzhivko@gmail.com>
2017-03-05 22:09:39 +0200

    Update view.php
    Put padding for text in view.

--- views/home/view.php
+++ views/home/view.php
@@ -2,3 +2,12 @@
 <p><strong>Posted on:</strong> <?=(new DateTime($this->review['date']))->format('d-M-Y')?></p>
 <p><?=$this->review['content']?></p>
 
+<style>
+    html {
+        padding: 1.5%;
+    }
+</style>
+
+
+
+
