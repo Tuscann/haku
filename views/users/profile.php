22b705d9450777799061f7778d9038d44a983ebb
NikolaNikolov <whitestarhs@gmail.com>
2017-03-09 18:25:58 +0200

    - Added comments to reviews

---
+++ views/users/profile.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+        <img class="profile-pic" src="<?=$this->user['profile-pic']?>" alt="">
+        <h2><?=$this->user['username']?></h2>
+        </div>
+
+    </div>
+    <div class="col-md-4">
+
+    </div>
+</div>
