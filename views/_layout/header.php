93172359917938425ef6a8f4d2d4ba620e1cfda5
xenonbg <anton.atanasov86@gmail.com>
2017-03-10 17:47:27 +0200

    added some dropdown styles

--- views/_layout/header.php
+++ views/_layout/header.php
@@ -11,7 +11,7 @@
     <title>Game reviews</title>
     <link href="<?= APP_ROOT ?>/content/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= APP_ROOT ?>/content/css/jumbotron.css" rel="stylesheet">
-    <link href="<?= APP_ROOT ?>/content/css/navbar.css" rel="stylesheet">
+
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -20,6 +20,7 @@
     <script src="<?= APP_ROOT ?>/content/js/functions.js"></script>
     <link rel="stylesheet" href="<?= APP_ROOT ?>/content/css/style.css">
     <link rel="stylesheet" type="text/css" href="<?= APP_ROOT ?>/content/css/view.css">
+    <link href="<?= APP_ROOT ?>/content/css/navbar.css" rel="stylesheet">
 
 
     <script src="<?= APP_ROOT ?>/content/js/jquery-1.9.1.min.js"></script>
